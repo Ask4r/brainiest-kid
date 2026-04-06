2fdb3d28eb6886a7c2eec41852c5cf9f810459b4
Ask4r <dv4askar@gmail.com>
2026-04-07 02:36:57 +0300

    Add QR code dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
+import { qrcode } from "vite-plugin-qrcode";
 import { resolve } from "path";
 
 // https://vite.dev/config/
@@ -10,6 +11,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
     VitePWA({ registerType: "autoUpdate" }),
+    qrcode(), // I'm so lazy...
   ],
   resolve: {
     alias: {
